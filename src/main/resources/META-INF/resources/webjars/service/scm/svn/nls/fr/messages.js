e30ec79bbbf8faff198a8d05d9c085782ddd2603
Fabrice Daugan <fabrice.daugan@gmail.com>
2017-10-03 13:15:54 +0200

    Fix missing i18n text for index parameter

--- src/main/resources/META-INF/resources/webjars/service/scm/svn/nls/fr/messages.js
+++ src/main/resources/META-INF/resources/webjars/service/scm/svn/nls/fr/messages.js
@@ -3,6 +3,7 @@ define({
 	'service:scm:svn:repository': 'Dépôt',
 	'service:scm:svn:user': 'Utilisateur',
 	'service:scm:svn:password': 'Mot de passe',
+	'service:scm:svn:index': 'URL est un index',
 	'error': {
 		'svn-admin': 'Accès administrateur échoué',
 		'svn-repository': 'Dépôt Subversion invalide'
--- src/main/resources/META-INF/resources/webjars/service/scm/svn/nls/messages.js
+++ src/main/resources/META-INF/resources/webjars/service/scm/svn/nls/messages.js
@@ -5,6 +5,7 @@ define({
 		'service:scm:svn:repository': 'Repository',
 		'service:scm:svn:user': 'User',
 		'service:scm:svn:password': 'Password',
+		'service:scm:svn:index': 'URL is index',
 		'error': {
 			'svn-admin': 'Administrator access failed',
 			'svn-repository': 'Invalid Subversion repository'
